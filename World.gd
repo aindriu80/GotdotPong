b0a70fd66553b218e5aa8aee1936bcfdcf25b766
aindriu80 <aindriu80@gmail.com>
2017-10-13 10:33:27 +0100

    Stopping left/right pads from leaving screen

--- World.gd
+++ World.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 #Constants
-const PLAYERSPEED = 200
+const PLAYERSPEED = 400
+
+#Variables
+var screenSize
 
 func _ready():		
+	screenSize = get_viewport_rect().size
 	set_process(true)
 	pass
 
@@ -13,13 +17,13 @@ func _process(delta):
 	var leftPlayerPosition = get_node("leftPlayer").get_pos()
 	
 	#Check input and move the pads
-	if(Input.is_action_pressed("ui_up")):
+	if(rightPlayerPosition.y > 0 and Input.is_action_pressed("ui_up")):
 		rightPlayerPosition.y += -PLAYERSPEED * delta
-	if(Input.is_action_pressed("ui_down")):
+	if(rightPlayerPosition.y < screenSize.y and Input.is_action_pressed("ui_down")):
 		rightPlayerPosition.y += PLAYERSPEED * delta
-	if(Input.is_action_pressed("left_up")):
+	if(leftPlayerPosition.y > 0 and  Input.is_action_pressed("left_up")):
 		leftPlayerPosition.y += -PLAYERSPEED * delta
-	if(Input.is_action_pressed("left_down")):
+	if(leftPlayerPosition.y< screenSize.y and Input.is_action_pressed("left_down")):
 		leftPlayerPosition.y += PLAYERSPEED * delta
 	
 	#Update the position of our players
