b5425a3f47450fdeb6592667d7c6db72807ae0dd
aindriu80 <aindriu80@gmail.com>
2017-10-13 10:10:41 +0100

    Pong Pad Movement

--- World.gd
+++ World.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
-
-
+const PLAYERSPEED = 200
 
 func _ready():		
 	set_process(true)
 	pass
 
+func _process(delta):
+	var rightPlayerPosition = get_node("rightPlayer").get_pos()
+	if(Input.is_action_pressed("ui_up")):
+		rightPlayerPosition.y += -PLAYERSPEED * delta
+	if(Input.is_action_pressed("ui_down")):
+		rightPlayerPosition.y += PLAYERSPEED * delta
 
+	get_node("rightPlayer").set_pos(rightPlayerPosition)
