fcef42ca1b36f94e24275d261699b70ead9fbcb5
aindriu80 <aindriu80@gmail.com>
2017-10-14 10:32:56 +0100

    ball movement and refelection

--- World.gd
+++ World.gd
@@ -2,16 +2,26 @@ extends Node2D
 
 #Constants
 const PLAYERSPEED = 400
+const INITBALLSPEED = 100
 
 #Variables
 var screenSize
+var padSize
+var ballDirection = Vector2(1.0,0.0)
+var ballSpeed = INITBALLSPEED
 
 func _ready():		
 	screenSize = get_viewport_rect().size
+	padSize = get_node("rightPlayer").get_texture().get_size()
 	set_process(true)
 	pass
 
 func _process(delta):
+	#ball position
+	var ballPosition = get_node("ball").get_pos()
+	#colliders
+	var leftColider = Rect2(get_node("leftPlayer").get_pos() - padSize*0.5, padSize)
+	var rightColider = Rect2(get_node("rightPlayer").get_pos() - padSize*0.5, padSize)
 	#variables for players positions
 	var rightPlayerPosition = get_node("rightPlayer").get_pos()
 	var leftPlayerPosition = get_node("leftPlayer").get_pos()
@@ -25,7 +35,16 @@ func _process(delta):
 		leftPlayerPosition.y += -PLAYERSPEED * delta
 	if(leftPlayerPosition.y< screenSize.y and Input.is_action_pressed("left_down")):
 		leftPlayerPosition.y += PLAYERSPEED * delta
+		
+	ballPosition += ballDirection * ballSpeed * delta
+	if((ballPosition.y <0 and ballDirection < 0) or (ballPosition.y > screenSize.y and ballDirection.y >0)):
+		ballDirection.y = -ballDirection.y
+	if(leftColider.has_point(ballPosition) or rightColider.has_point(ballPosition)):
+		ballDirection.x = -ballDirection.x
+		ballSpeed *= 1.4
+		
 	
 	#Update the position of our players
 	get_node("rightPlayer").set_pos(rightPlayerPosition)
 	get_node("leftPlayer").set_pos(leftPlayerPosition)
+	get_node("ball").set_pos(ballPosition)
